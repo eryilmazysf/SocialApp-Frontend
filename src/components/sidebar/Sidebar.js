70800f54fc1d4e740040745e674cb79246451826
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-07-27 23:55:39 -0400

    created share component

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -53,6 +53,18 @@ export default function Sidebar() {
             <span className="sidebarListItemText">Courses</span>
           </li>
         </ul>
+        <button className="sidebarButton">Show More</button>
+        <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          <li className="sidebarFriend">
+            <img
+              src="/assets/person/2.jpeg"
+              alt="yusuf"
+              className="sidebarFriendImg"
+            />
+            <span className="sidebarFriendName">Yusuf </span>
+          </li>
+        </ul>
       </div>
     </div>
   );
